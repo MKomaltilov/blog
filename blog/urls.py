58a990308ed145e0b656d008d9453bcc679f67df
MKomaltilov <marat.komaltilov@icloud.com>
2020-10-15 21:37:45 +0700

    page for Article added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import ArticlesView
+from blog.views import ArticlesView, article_view
 
 urlpatterns = [
     path('', ArticlesView.as_view(), name='articles'),
+    path('articles/<int:article_id>', article_view, name='article_view')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.shortcuts import render
 
 from blog.models import Article
 
@@ -7,3 +8,9 @@ class ArticlesView(ListView):
     model = Article
     queryset = Article.objects.filter(is_published=True)
     template_name = 'articles/overview.html'
+
+
+def article_view(request, article_id):
+    article = Article.objects.get(pk=article_id)
+
+    return render(request, 'articles/article.html', {"article": article})

259eb04ba22457f38c3ba200932c6fc5dda19581
MKomaltilov <marat.komaltilov@icloud.com>
2020-10-19 19:34:36 +0700

    view for tag added

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import ArticlesView, article_view
+from blog.views import ArticlesView, article_view, tag_view
 
 urlpatterns = [
     path('', ArticlesView.as_view(), name='articles'),
-    path('articles/<int:article_id>', article_view, name='article_view')
+    path('articles/<int:article_id>', article_view, name='article_view'),
+    path('tags/<uuid:tag_id>', tag_view, name='tag_view')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from blog.models import Article
+from blog.models import Article, Tag
 
 
 class ArticlesView(ListView):
@@ -14,3 +14,9 @@ def article_view(request, article_id):
     article = Article.objects.get(pk=article_id)
 
     return render(request, 'articles/article.html', {"article": article})
+
+
+def tag_view(request, tag_id):
+    tag = Tag.objects.get(pk=tag_id)
+
+    return render(request, 'tags/tag.html', {"tag": tag})
